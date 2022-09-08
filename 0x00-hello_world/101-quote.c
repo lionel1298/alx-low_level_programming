7122f7aaba2a203bd612cd36d62a87a3eaf6ad12
margaret kavithe <margaretkavithe@gmail.com>
2022-09-08 12:03:04 -0700

    did it again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always ONE (Success)
+ * Return: Always one  (Success)
  */
 int main (void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);  
-return (1);
+return (1)
 }

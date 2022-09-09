68ddb4dbe414348dd8bd9aff3458c1c6e4bcc262
margaret kavithe <margaretkavithe@gmail.com>
2022-09-09 15:04:25 -0700

    prints alphabets in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,7 +5,7 @@
  *  Description - a program that prints the lowercase alphabet in reverse,
  *  followed by a new line.
  *
- *  Condition 
+ *  Condition
  *  You can only use the putchar function (every other function
  *  (printf, puts, etc…) is forbidden),
  *  All your code should be in the main function,

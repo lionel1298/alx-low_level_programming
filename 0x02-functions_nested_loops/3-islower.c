dd63c054369831e45bfa08beb3f1431c72846137
margaret kavithe <margaretkavithe@gmail.com>
2022-09-14 06:40:07 -0700

    the lower case only

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,11 @@
 /**
  * _islower - check for lowercase
  *
+ * @c: integer value which is an ascii number of a character
+ *
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  *
- * Return: 1 if is lowercase, else 0
+ * Return: 1 if c is lowercase, else 0
  */
 int _islower(int c)
 {

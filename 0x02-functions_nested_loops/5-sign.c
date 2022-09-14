6a2024b26e69e70679b623fb143406643707871f
margaret kavithe <margaretkavithe@gmail.com>
2022-09-14 07:04:33 -0700

    add sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - Sign detection
+ *
+ * @n: parameter is an integer value
+ *
+ * Description: a function that prints the sign of a number.
+ *
+ * Return: 1 if n > 0, 0 if n equals 0 and -1 if n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	elseif (n==0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

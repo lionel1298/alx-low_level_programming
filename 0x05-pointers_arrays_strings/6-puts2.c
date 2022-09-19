7ac775ff35941f6a09577ef59d54716703b9206d
margaret kavithe <margaretkavithe@gmail.com>
2022-09-19 13:21:28 -0700

    pritns every character of a sting

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints even characters
+ * @str: string parameter
+ * Description: a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *  Return: None
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
